edebc81c9870c3c1f0bf3b2da4b9b797f71a05d9
Real Hadyan <realhadyanfico@gmail.com>
2026-08-22 19:11:54 +0700

    mod: add report test

--- internal/assets/assets_test.go
+++ internal/assets/assets_test.go
@@ -4,6 +4,7 @@ import (
 	"RewriteProject/internal/assets"
 	"RewriteProject/internal/config"
 	"RewriteProject/internal/localstorage"
+	"RewriteProject/internal/utils"
 	"bytes"
 	"io"
 	"testing"
@@ -53,6 +54,58 @@ type DeleteTestPayload struct {
 	AssetID     []string
 }
 
+type TestReport struct {
+	Pass        bool
+	Name        string
+	Error       error
+	ExpectedErr error
+}
+
+type Mime string
+
+const (
+	//  Image Mime Allowed
+	MimeImageJPEG Mime = "image/jpeg"
+	MimeImagePNG  Mime = "image/png"
+	MimeImageGIF  Mime = "image/gif"
+	MimeImageWebP Mime = "image/webp"
+
+	// Video
+	MimeVideoMP4  Mime = "video/mp4"
+	MimeVideoMOV  Mime = "video/quicktime"
+	MimeVideoAVI  Mime = "video/x-msvideo"
+	MimeVideoMKV  Mime = "video/x-matroska"
+	MimeVideoWebM Mime = "video/webm"
+	MimeVideoFLV  Mime = "video/x-flv"
+
+	// Document
+	MimeDocPDF  Mime = "application/pdf"
+	MimeDocDocx Mime = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+	MimeDocXlsx Mime = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+	MimeDocPptx Mime = "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+	MimeDocDoc  Mime = "application/msword"
+)
+
+var TestBuffer = map[Mime][]byte{
+	MimeImageJPEG: {0xFF, 0xD8, 0xFF, 0xE0},
+	MimeImagePNG:  {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A},
+	MimeImageGIF:  []byte("GIF89a"),
+	MimeImageWebP: append([]byte("RIFF"), append([]byte{0x00, 0x00, 0x00, 0x00}, []byte("WEBP")...)...),
+
+	MimeDocDoc:  {0xD0, 0xCF, 0x11, 0xE0, 0xA1, 0xB1, 0x1A, 0xE1},
+	MimeDocDocx: {0x50, 0x4B, 0x03, 0x04},
+	MimeDocPptx: {0x50, 0x4B, 0x03, 0x04},
+	MimeDocXlsx: {0x50, 0x4B, 0x03, 0x04},
+	MimeDocPDF:  []byte("%PDF-1.4"),
+
+	MimeVideoMP4:  {0x00, 0x00, 0x00, 0x18, 0x66, 0x74, 0x79, 0x70, 0x6D, 0x70, 0x34, 0x32},
+	MimeVideoMOV:  {0x00, 0x00, 0x00, 0x14, 0x66, 0x74, 0x79, 0x70, 0x71, 0x74, 0x20, 0x20},
+	MimeVideoAVI:  {0x52, 0x49, 0x46, 0x46, 0x00, 0x00, 0x00, 0x00, 0x41, 0x56, 0x49, 0x20},
+	MimeVideoMKV:  {0x1A, 0x45, 0xDF, 0xA3},
+	MimeVideoWebM: {0x1A, 0x45, 0xDF, 0xA3},
+	MimeVideoFLV:  {0x46, 0x4C, 0x56, 0x01},
+}
+
 func NewUC(t *testing.T) assets.UsecaseContract {
 	conf := config.StorageConfig{
 		StorageRoot:         t.TempDir(),
@@ -73,6 +126,7 @@ func NewUC(t *testing.T) assets.UsecaseContract {
 	}
 	return assetManager
 }
+
 func CreateUploadUsecase() []UploadTestPayload {
 	return []UploadTestPayload{
 		{
@@ -84,51 +138,18 @@ func CreateUploadUsecase() []UploadTestPayload {
 				MaxSize:  1024 * 1024 * 10,
 				UserID:   "VALID_CATEGORY_UPLOAD_TEST",
 			},
-			Reader: bytes.NewBuffer([]byte("%PDF-1.4")),
+			Reader: bytes.NewBuffer(TestBuffer[MimeDocPDF]),
 		},
 		{
 			Name:        "VALID_CATEGORY_UPLOAD_TEST",
-			ExpectedErr: nil,
-			Usecase:     Upload,
-			Policy: assets.UploadPolicy{
-				Category: assets.CategoryDocument,
-				MaxSize:  1024 * 1024 * 10,
-				UserID:   "VALID_CATEGORY_UPLOAD_TEST",
-			},
-			Reader: bytes.NewBuffer([]byte("%PDF-1.4")),
-		},
-		{
-			Name:        "VALID_CATEGORY_UPLOAD_TEST",
-			ExpectedErr: nil,
-			Usecase:     Upload,
-			Policy: assets.UploadPolicy{
-				Category: assets.CategoryDocument,
-				MaxSize:  1024 * 1024 * 10,
-				UserID:   "VALID_CATEGORY_UPLOAD_TEST",
-			},
-			Reader: bytes.NewBuffer([]byte("%PDF-1.4")),
-		},
-		{
-			Name:        "INVALID_CATEGORY_UPLOAD_TEST",
 			ExpectedErr: assets.ErrAssetInvalidMime,
 			Usecase:     Upload,
 			Policy: assets.UploadPolicy{
-				Category: assets.AssetCategory("invalid"),
-				MaxSize:  1024 * 1024 * 10,
-				UserID:   "INVALID_CATEGORY_UPLOAD_TEST",
-			},
-			Reader: bytes.NewBuffer([]byte("%PDF-1.4")),
-		},
-		{
-			Name:        "INVALID_SIZE_UPLOAD_TEST",
-			ExpectedErr: assets.ErrAssetTooLarge,
-			Usecase:     Upload,
-			Policy: assets.UploadPolicy{
 				Category: assets.CategoryDocument,
-				MaxSize:  1,
-				UserID:   "INVALID_SIZE_UPLOAD_TEST",
+				MaxSize:  1024 * 1024 * 10,
+				UserID:   "VALID_CATEGORY_UPLOAD_TEST",
 			},
-			Reader: bytes.NewBuffer([]byte("%PDF-1.4")),
+			Reader: bytes.NewBuffer(TestBuffer[MimeVideoMP4]),
 		},
 		{
 			Name:        "INVALID_READER_CATEGORY_UPLOAD_TEST",
@@ -173,6 +194,8 @@ func TestUpload(t *testing.T) {
 	var validIDS []string
 	var deleteTestPayload []DeleteTestPayload
 
+	var Report []TestReport
+
 	validAssets = append(validAssets, ReadTestPayload{
 		Name:        "EMPTY_FILE_KEY_READ_TEST",
 		ExpectedErr: assets.ErrAssetNotFound,
@@ -202,16 +225,40 @@ func TestUpload(t *testing.T) {
 				validIDS = append(validIDS, res.ID)
 			}
 			if err != tc.ExpectedErr {
+				Report = append(Report, TestReport{
+					Pass:        false,
+					Name:        tc.Name,
+					Error:       err,
+					ExpectedErr: tc.ExpectedErr,
+				})
 				t.Fatalf("Upload: expected =%v, got err=%v", tc.ExpectedErr, err)
 			}
+			Report = append(Report, TestReport{
+				Pass:        true,
+				Name:        tc.Name,
+				Error:       nil,
+				ExpectedErr: nil,
+			})
 		})
 	}
 	for _, tc := range validAssets {
 		t.Run(tc.Name, func(t *testing.T) {
 			f, err := assetsUC.Read(t.Context(), tc.FileKey)
 			if err != tc.ExpectedErr {
+				Report = append(Report, TestReport{
+					Pass:        false,
+					Name:        tc.Name,
+					Error:       err,
+					ExpectedErr: tc.ExpectedErr,
+				})
 				t.Fatalf("ReadByID: expected =%v, got err=%v", tc.ExpectedErr, err)
 			}
+			Report = append(Report, TestReport{
+				Pass:        true,
+				Name:        tc.Name,
+				Error:       nil,
+				ExpectedErr: nil,
+			})
 			if f != nil {
 				defer f.Close()
 			}
@@ -244,14 +291,38 @@ func TestUpload(t *testing.T) {
 		t.Run(tc.Name, func(t *testing.T) {
 			err := assetsUC.LinkingAsset(t.Context(), tc.AssetID, tc.ParentID)
 			if err != tc.ExpectedErr {
+				Report = append(Report, TestReport{
+					Pass:        false,
+					Name:        tc.Name,
+					Error:       err,
+					ExpectedErr: tc.ExpectedErr,
+				})
 				t.Fatalf("LinkingAsset: expected =%v, got err=%v", tc.ExpectedErr, err)
 			}
+			Report = append(Report, TestReport{
+				Pass:        true,
+				Name:        tc.Name,
+				Error:       nil,
+				ExpectedErr: nil,
+			})
 		})
 		t.Run(tc.Name+"_READ_BY_PARENT_ID", func(t *testing.T) {
 			_, err := assetsUC.ReadByParentID(t.Context(), []string{tc.ParentID})
 			if err != tc.ExpectedErr {
+				Report = append(Report, TestReport{
+					Pass:        false,
+					Name:        tc.Name + "_READ_BY_PARENT_ID",
+					Error:       err,
+					ExpectedErr: tc.ExpectedErr,
+				})
 				t.Fatalf("ReadByParentID: expected =%v, got err=%v", tc.ExpectedErr, err)
 			}
+			Report = append(Report, TestReport{
+				Pass:        true,
+				Name:        tc.Name + "_READ_BY_PARENT_ID",
+				Error:       nil,
+				ExpectedErr: nil,
+			})
 		})
 
 	}
@@ -259,8 +330,20 @@ func TestUpload(t *testing.T) {
 		t.Run(tc.Name+"_RELEASE_BY_PARENT_ID", func(t *testing.T) {
 			err := assetsUC.ReleaseByParentID(t.Context(), []string{tc.ParentID})
 			if err != tc.ExpectedErr {
+				Report = append(Report, TestReport{
+					Pass:        false,
+					Name:        tc.Name + "_RELEASE_BY_PARENT_ID",
+					Error:       err,
+					ExpectedErr: tc.ExpectedErr,
+				})
 				t.Fatalf("ReleaseByParentID: expected =%v, got err=%v", tc.ExpectedErr, err)
 			}
+			Report = append(Report, TestReport{
+				Pass:        true,
+				Name:        tc.Name + "_RELEASE_BY_PARENT_ID",
+				Error:       nil,
+				ExpectedErr: nil,
+			})
 		})
 	}
 
@@ -286,9 +369,37 @@ func TestUpload(t *testing.T) {
 		t.Run(tc.Name, func(t *testing.T) {
 			err := assetsUC.Delete(t.Context(), assets.DeletePayload{IDs: tc.AssetID})
 			if err != tc.ExpectedErr {
+				Report = append(Report, TestReport{
+					Pass:        false,
+					Name:        tc.Name,
+					Error:       err,
+					ExpectedErr: tc.ExpectedErr,
+				})
 				t.Fatalf("Delete: expected =%v, got err=%v", tc.ExpectedErr, err)
 			}
+			Report = append(Report, TestReport{
+				Pass:        true,
+				Name:        tc.Name,
+				Error:       nil,
+				ExpectedErr: nil,
+			})
 		})
 	}
+
+	PassReaport := utils.MapField(Report, func(tr TestReport) (TestReport, bool) {
+		if tr.Pass {
+			return tr, true
+		}
+		return tr, false
+	})
+	FailedReport := utils.MapField(Report, func(tr TestReport) (TestReport, bool) {
+		if !tr.Pass {
+			return tr, true
+		}
+		return tr, false
+	})
+	t.Log("Total Test: ", len(Report))
+	t.Log("Pass Test: ", len(PassReaport))
+	t.Log("Failed Test: ", len(FailedReport))
 
 }
